dbc9b440af62eeec28118ccf89b31e75eb8c9765
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-11-05 08:51:12 +0100

    player

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,129 +1,126 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-    </v-row>
+  <v-container fluid>
+    {{secondTiming}}
+    <v-card
+        class="mx-auto"
+        max-width="450"
+    >
+      <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="400px"
+      ></v-img>
+      <v-row dense class="d-flex justify-center">
+        <v-col class="d-flex justify-center flex-column align-center">
+          <p class="pa-0 ma-0">{{morceau}}</p>
+          <p class="pa-0 ma-0">{{artist}}</p>
+          <p v-if="startingSong && duration "> {{startingSong}} / {{duration}}</p>
+        </v-col>
+        <v-col class="d-flex justify-center align-center">
+          <v-card-actions>
+            <v-btn
+                class="mx-2"
+                fab
+                dark
+                small
+                color="blue"
+            >
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+            </v-btn>
 
-    <h1>Hello la promo Ynov !</h1>
-    <h3>Bienvenue sur Ynov Songs :)</h3>
+            <!-- play-->
+            <v-btn
+                v-if="!played"
+                class="mx-2"
+                fab
+                dark
+                small
+                @click="startSong"
+                color="blue"
+            >
+              <v-icon>
+                mdi-play
+              </v-icon>
+            </v-btn>
 
-    <button @click="loaded = true">Chargement des Todos</button>
-    <Todos v-if="loaded"></Todos>
+            <v-btn
+                v-else
+                class="mx-2"
+                fab
+                dark
+                small
+                @click="stopSong"
+                color="blue"
+            >
+              <v-icon>
+                mdi-pause
+              </v-icon>
+            </v-btn>
 
-    <p>Nb de count: {{ count }}</p>
-    <button @click="add" class="btn">Ajouter une plus !</button>
-    <button @click="remove" class="btn">Diminuer une plus !</button>
+            <!--stop -->
+            <v-btn
+                class="mx-2"
+                fab
+                dark
+                small
+                color="blue"
+            >
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+            </v-btn>
 
-    <p v-if="count >= 10">Win!</p>
-    <p v-else-if="count > 5">Win!!!!!!</p>
-    <p v-else>Loose!</p>
-
-    <h1 v-show="count > 10">Hello!</h1>
-
-    <h4>Prénom:</h4>
-
-    <input v-model="nbDisplay" type="range" min="0" max="6" />
-    <input
-      v-model="search"
-      type="search"
-      required
-      placeholder="recherche un prénom"
-    />
-    <p :class="colorNb">
-      Nb de prénom<span v-if="prenoms.length > 1">s</span>: {{ prenoms.length }}
-    </p>
-    <input
-      v-model="prenom"
-      type="text"
-      required
-      placeholder="Ajouter un prénom"
-    />
-    <button @click="ajouterPrenom">Ajouter</button>
-    <p>Nb de stars: {{ nbLike }} likes</p>
-    <p v-if="goodColor">Vous êtes une star !</p>
-    <Liste :filterPrenoms="filterPrenoms">
-      <template v-slot:header>
-        <h1>Liste des {{ filterPrenoms.length }} prénoms</h1>
-      </template>
-    </Liste>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
+
 </template>
 
 <script>
-import Liste from "./Liste"; // 1 import
-import Todos from "./Todos";
+
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
   data: function () {
     return {
-      loaded: false,
-      count: 0,
-      colorNb: "blue",
-      prenom: "",
-      search: "",
-      nbDisplay: 3,
-      prenoms: ["Steven", "Julien", "Guillaume", "Baptiste"],
-    };
+      sound: '',
+      played: false,
+      artist: 'Jul',
+      morceau: 'En Y !',
+      duration: '',
+      secondTiming:'',
+      startingSong:' 00:00 ',
+
+    }
   },
   methods: {
-    load() {
-      this.loaded = true;
+    startSong(){
+      this.sound.play();
+      this.secondTiming = this.sound.duration
+      this.played = true
+      this.ParsingTiming();
     },
-    ajouterPrenom() {
-      this.prenoms.push(this.prenom);
-      this.prenom = "";
-      if (this.prenoms.length >= 15) {
-        this.colorNb = "red";
-      }
+    stopSong(){
+      this.sound.pause();
+      this.played = false
     },
-    add() {
-      this.count++;
+    ParsingTiming() {
+        let minutes = Math.floor(this.secondTiming / 60);
+        let secs = this.secondTiming%60;
+        let hours = Math.floor(minutes/60)
+        minutes = minutes%60;
+        this.duration = (`${this.pad(hours)}:${this.pad(minutes)}:${this.pad(secs)}`);
     },
-    remove() {
-      this.count--;
-    },
-  },
-  components: {
-    Liste, // 2 use
-    Todos,
-  },
-  computed: {
-    nbLike() {
-      return this.prenoms.length * this.count;
-    },
-    goodColor() {
-      return this.nbLike > 30;
-    },
-    filterPrenoms() {
-      if (this.search.length < 3) {
-        return this.prenoms;
-      } else {
-        return this.prenoms.filter((elt) => elt.includes(this.search));
-      }
+    pad(num) {
+      return ("0"+num).slice(-2);
     },
   },
-  watch: {
-    nbLike(val) {
-      if (val > 40) {
-        this.prenoms = [];
-      }
-    },
-    nbDisplay(val) {
-      this.prenoms = this.prenoms.slice(0, val);
-      if (val == 0) {
-        this.prenoms = ["Steven", "Julien", "Guillaume", "Baptiste"];
-      }
-    },
-  },
+  computed: {},
+  watch: {},
+  created() {
+    this.sound = new Audio(require("@/assets/JUL - EN Y _ CLIP OFFICIEL _ D'OR ET DE PLATINE _ 2015.mp3"));
+  }
 };
 </script>
