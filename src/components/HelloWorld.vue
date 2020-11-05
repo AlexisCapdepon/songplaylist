e1ba2bb362dcf08dc773729d031253f1c4e76733
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-11-05 14:20:21 +0100

    update card style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,74 +5,91 @@
         max-width="350"
     >
       <v-img
+          height="200px"
           :src="require('../assets/' + current.picture)"
-          height="250px"
-      ></v-img>
-      <v-row dense class="d-flex justify-center">
-        <v-col class="d-flex justify-center flex-column align-center">
-          <p class="pa-0 ma-0">{{ current.track }}</p>
-          <p class="pa-0 ma-0">{{ current.artist }}</p>
-          <p v-if="current.trackDuration"> {{ datingTime }} / {{ current.trackDuration }}</p>
-        </v-col>
-        <v-col class="d-flex justify-center align-center">
-          <v-card-actions>
-            <v-btn
-                class="mx-2"
-                fab
-                dark
-                small
-                color="blue"
-                @click="backSong"
-            >
-              <v-icon dark>
-                mdi-skip-backward
-              </v-icon>
-            </v-btn>
+      >
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+        >
+          <v-spacer></v-spacer>
 
-            <!-- play-->
-            <v-btn
-                v-if="!played"
-                class="mx-2"
-                fab
-                small
-                @click="startSong"
-                color="blue"
-            >
-              <v-icon dark>
-                mdi-play
-              </v-icon>
-            </v-btn>
+          <v-btn
+              color="white"
+              icon
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+      </v-img>
 
-            <v-btn
-                v-else
-                class="mx-2"
-                fab
-                dark
-                small
-                @click="stopSong"
-                color="blue"
-            >
-              <v-icon dark>
-                mdi-pause
-              </v-icon>
-            </v-btn>
+      <v-card-text>
+        <v-row dense class="d-flex justify-center">
+          <v-col class="d-flex justify-center flex-column align-center">
+            <p class="pa-0 ma-0">{{ current.track }}</p>
+            <p class="pa-0 ma-0">{{ current.artist }}</p>
+            <p v-if="current.trackDuration"> {{ datingTime }} / {{ current.trackDuration }}</p>
+          </v-col>
+          <v-col class="d-flex justify-center align-center">
+            <v-card-actions>
+              <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  small
+                  color="blue"
+                  @click="backSong"
+              >
+                <v-icon dark>
+                  mdi-skip-backward
+                </v-icon>
+              </v-btn>
 
-            <!--stop -->
-            <v-btn
-                class="mx-2"
-                fab
-                dark
-                small
-                color="blue"
-                @click="nextSong"
-            >
-              <v-icon dark>
-                mdi-skip-forward
-              </v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-col>
-      </v-row>
+              <!-- play-->
+              <v-btn
+                  v-if="!played"
+                  class="mx-2"
+                  fab
+                  small
+                  @click="startSong"
+                  color="blue"
+              >
+                <v-icon dark>
+                  mdi-play
+                </v-icon>
+              </v-btn>
+
+              <v-btn
+                  v-else
+                  class="mx-2"
+                  fab
+                  dark
+                  small
+                  @click="stopSong"
+                  color="blue"
+              >
+                <v-icon dark>
+                  mdi-pause
+                </v-icon>
+              </v-btn>
+
+              <!--stop -->
+              <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  small
+                  color="blue"
+                  @click="nextSong"
+              >
+                <v-icon dark>
+                  mdi-skip-forward
+                </v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card>
   </v-container>
 
@@ -95,11 +112,13 @@ export default {
         },
         {
           url: 'JUL - EN Y _ CLIP OFFICIEL _ D\'OR ET DE PLATINE _ 2015.mp3',
+          picture: 'jul.jpg',
           artist: 'Ju',
           track: 'EnY !',
         },
         {
           url: 'JUL - EN Y _ CLIP OFFICIEL _ D\'OR ET DE PLATINE _ 2015.mp3',
+          picture: 'jul.jpg',
           artist: 'l',
           track: ' Y !',
         },
